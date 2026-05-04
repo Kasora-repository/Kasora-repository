b683f08eb373431dbcb4b26aa9d693b8f6258d00
James Gabriel B. Boo <boojamesgabriel@gmail.com>
2026-05-04 12:43:39 +0800

    styled the account log in overlay

--- kasorra-frontend/scripts/acc.js
+++ kasorra-frontend/scripts/acc.js
@@ -8,40 +8,66 @@ function acc_button() {
     let bg = document.createElement('div');
     bg.className = "acc_bg";
 
+    let lock = document.createElement('img');
+    lock.src = "images/lock2.png";
+    lock.className = "lock-icon";
+
     let login_header = document.createElement('p');
     login_header.className = "login_header";
     login_header.textContent = "Welcome Back";
+
+    let login_subheader = document.createElement('p');
+    login_subheader.className = "login_subheader";
+    login_subheader.textContent = "Sign in to your Account";
 
     let exit = document.createElement('button');
     exit.className = "exit_button";
     exit.textContent = "x";
     exit.onclick = close_overlay;
 
+    let input_container = document.createElement('div');
+    input_container.className = "input_container";
+
     let form = document.createElement('form');
     form.className = 'form_tag';
     form.onsubmit = handle_login; //function handle_login
 
+    let email_header = document.createElement('p');
+    email_header.className = "email_header";
+    email_header.textContent = "Email or Username";
+
     let email = document.createElement('input');
     email.type = "text";
     email.name = "email";
-    email.placeholder = "Please Enter your username or email";
+    email.placeholder = "Enter your username or email";
     email.className = "input_tag";
+
+    let password_header = document.createElement('p');
+    password_header.className = "password_header";
+    password_header.textContent = "Password";
 
     let password = document.createElement('input');
     password.type = "password";
     password.name = "password";
-    password.placeholder = "Please Enter your password";
+    password.placeholder = "Enter your password";
     password.className = "input_tag";
 
     let submit = document.createElement('button');
-    submit.textContent = "submit";
+    submit.textContent = "Sign In";
     submit.value = "Log In";
+    submit.className = "submit_button";
 
-    form.appendChild(email);
-    form.appendChild(password);
+    input_container.appendChild(email);
+    input_container.appendChild(password);
+    input_container.appendChild(email_header);
+    input_container.appendChild(password_header);
+
+    form.appendChild(input_container);
     form.appendChild(submit);
 
+    bg.appendChild(lock);
     bg.appendChild(login_header);
+    bg.appendChild(login_subheader);
     bg.appendChild(exit);
     bg.appendChild(form);
 
